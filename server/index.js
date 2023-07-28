d03ec05bfa44cb27c06ddfcdfa7812793dda64ef
Petr Ivanovskiy <peter.ivanovskij@gmail.com>
2023-07-28 15:04:36 +0300

    init the project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
